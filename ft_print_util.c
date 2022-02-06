70cae21027687276a52ab9d28bf5ed4752a0cc6f
HONGMINJU <mhong0800@gmail.com>
2022-02-07 01:50:43 +0900

    .

--- ft_print_util.c
+++ ft_print_util.c
@@ -59,9 +59,9 @@ int	print_xX(unsigned int num, int isUpper)
 	if (num >= 16)
 		cnt += print_xX(num / 16, isUpper);
 	if (isUpper)
-		ft_putchar_fd("0123456789ABCDEF"[num % 16], 1);
+		ft_putchar_fd("0123456789ABCDEF"[num % 16], isUpper);
 	else if (!isUpper)
-		ft_putchar_fd("0123456789abcdef"[num % 16], 1);
+		ft_putchar_fd("0123456789abcdef"[num % 16], isUpper);
 	return (cnt);
 }
 
