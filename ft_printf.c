71cb0819f216c3c3484aa4552f3871c55e4be4ee
HONGMINJU <mhong0800@gmail.com>
2022-02-07 01:37:01 +0900

    fix

--- ft_printf.c
+++ ft_printf.c
@@ -12,23 +12,23 @@
 
 #include "ft_printf.h"
 
-int	check(const char *str, va_list argList)
+int	check(char str, va_list argList)
 {
-	if (*str == 's')
+	if (str == 's')
 		return (print_s(va_arg(argList, char *)));
-	else if (*str == 'p')
+	else if (str == 'p')
 		return (print_p(va_arg(argList, long long)));
-	else if (*str == 'd' || *str == 'i')
+	else if (str == 'd' || str == 'i')
 		return (print_di(va_arg(argList, int)));
-	else if (*str == 'x')
+	else if (str == 'x')
 		return (print_xX(va_arg(argList, unsigned int), 0));
-	else if (*str == 'X')
+	else if (str == 'X')
 		return (print_xX(va_arg(argList, unsigned int), 1));
-	else if (*str == 'u')
+	else if (str == 'u')
 		return (print_u(va_arg(argList, unsigned int)));
-	else if (*str == 'c')
+	else if (str == 'c')
 		ft_putchar_fd(va_arg(argList, int), 1);
-	else if (*str == '%')
+	else if (str == '%')
 		ft_putchar_fd('%', 1);
 	return (1);
 }
@@ -44,11 +44,15 @@ int	ft_printf(const char *str, ...)
 	{
 		if (*str != '%')
 		{
+			ft_putchar_fd(*str, 1);
 			cnt++;
-			ft_putchar_fd(*str++, 1);
+			str++;
 		}
 		else
-			cnt += check(str++, argList);
+		{
+			str++;
+			cnt += check(*str, argList);
+		}
 	}
 	return (cnt);
 }
